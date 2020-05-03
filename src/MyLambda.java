f4f2e1ee239d8cf6f6e1ec1b78f920f99f89be0f
514515479 <514515479@qq.com>
2020-05-04 01:15:38 +0800

    Lambda表达式学习

--- src/MyLambda.java
+++ src/MyLambda.java
@@ -9,7 +9,7 @@ import java.util.Comparator;
 import java.util.function.Consumer;
 
 /**
- * 一、Lambda表达式基础语法：Java8中引入了一个新的操作符 “->”，该操作符称作为箭头操作符或者是Lambda操作符
+ * 一、Lambda表达式基础语法：Java8中引入了一个新的操作符 “->”，该操作符称作为箭头操作符或者是Lambda操作符。
  *
  *    箭头操作符讲Lambda表达式拆成两部分：
  *      左侧：Lambda 表达式的参数列表
@@ -32,6 +32,44 @@ import java.util.function.Consumer;
  *              (x, y) ->  Integer.compare(x, y);
  *
  *    语法格式五：Lambda表达式的参数列表的数据类型可以省略不写，因为JVM编译器通过上下文推断出数据类型，即“类型推断”
+ *
+ * 二、Lambda表达式需要“函数式接口”的支持
+ * 函数式接口：若接口中只有一个抽象方法的接口，成为函数式接口。
+ *           可以使用注解 @FunctionalInterface 修饰，可以检查是否是函数式接口。
+ *           加了@FunctionalInterface，只允许有一个抽象方法。接口中（interrface）的所有方法都是隐式public和abstract的（但不是final的）。
+
+
+ *           // 正确的函数式接口
+ *           @FunctionalInterface
+ *           public interface TestInterface {
+ *
+ *               // 抽象方法
+ *               public void sub();
+ *
+ *               // java.lang.Object中的public方法
+ *               public boolean equals(Object var1);
+ *
+ *               // 默认方法
+ *               public default void defaultMethod(){
+ *
+ *               }
+ *
+ *               // 静态方法
+ *               public static void staticMethod(){
+ *
+ *               }
+ *           }
+ *
+ *           // 错误的函数式接口(有多个抽象方法)
+ *           @FunctionalInterface
+ *           public interface TestInterface2 {
+ *
+ *               void add();
+ *
+ *               void sub();
+ *           }
+ *
+ *
  **/
 public class MyLambda {
 
