d97bb930ba22d27c5550646dfc0ed9d1acc796f5
514515479 <514515479@qq.com>
2020-05-04 00:02:32 +0800

    Lambda表达式学习

---
+++ src/MyLambda.java
@@ -0,0 +1,15 @@
+/**
+ * @Author: tobi
+ * @Date: 2020/5/3 23:48
+ **/
+
+/**
+ * 一、Lambda表达式基础语法：Java8中引入了一个新的操作符 “->”，该操作符称作为箭头操作符或者是Lambda操作符
+ *
+ *    箭头操作符讲Lambda表达式拆成两部分：
+ *      左侧：Lambda 表达式的参数列表
+ *      右侧：Lambda 表达式执行的功能，即Lambda体
+ **/
+public class MyLambda {
+
+}
