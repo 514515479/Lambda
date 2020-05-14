bb76c3d65b6aadaa34521f8dab06081cfbb935a4
514515479 <514515479@qq.com>
2020-05-14 13:46:23 +0800

    Create Trader.java

---
+++ src/Trader.java
@@ -0,0 +1,19 @@
+import lombok.Data;
+
+/**
+ * 交易员类
+ **/
+@Data
+public class Trader {
+	private String name;
+	private String city;
+
+	public Trader() {
+
+	}
+
+	public Trader(String name, String city) {
+		this.name = name;
+		this.city = city;
+	}
+}
