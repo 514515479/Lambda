e60c1a067afa39a3c6fc130b9d5dd5e12cc09732
514515479 <514515479@qq.com>
2020-06-09 17:35:54 +0800

    Create TestDefaultStatic.java

---
+++ src/TestDefaultStatic.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        TestDefault testInterfaceClass = new TestDefault();
+        System.out.println(testInterfaceClass.getName());
+
+        System.out.println(MyInterface.name());
+    }
+
+}
