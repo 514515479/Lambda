49d483fcefaa64602622a43caebdcc311bb905e3
514515479 <514515479@qq.com>
2020-06-11 14:07:49 +0800

    Create TestSimplDateFormat.java

---
+++ src/TestSimplDateFormat.java
@@ -0,0 +1,33 @@
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class TestSimplDateFormat {
+
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		Callable<Date> task = () ->  sdf.parse("2020-06-12");
+		/*Callable<Date> task = new Callable<Date>() {
+			@Override
+			public Date call() throws Exception {
+				return sdf.parse("2020-06-12");
+			}
+		};*/
+
+		List<Future<Date>> list = new ArrayList<>();
+
+		ExecutorService pool = Executors.newFixedThreadPool(10);
+
+		for (int i = 0; i < 100; i++) {
+			list.add(pool.submit(task));
+		}
+
+		for (Future<Date> result : list) {
+			System.out.println(result.get());
+		}
+	}
+}
