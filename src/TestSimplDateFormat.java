ec684702dbfa1208ff119f28316f839328372522
514515479 <514515479@qq.com>
2020-06-11 14:44:42 +0800

    Update TestSimplDateFormat.java

--- src/TestSimplDateFormat.java
+++ src/TestSimplDateFormat.java
@@ -1,5 +1,7 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -7,18 +9,18 @@ import java.util.concurrent.*;
 
 public class TestSimplDateFormat {
 
-	public static void main(String[] args) throws ExecutionException, InterruptedException, ParseException {
+	/*public static void main(String[] args) throws ExecutionException, InterruptedException, ParseException {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
 		//线程不安全
 		//Callable<Date> task = () ->  sdf.parse("2020-06-12");
-		/*Callable<Date> task = new Callable<Date>() {
+		*//*Callable<Date> task = new Callable<Date>() {
 			@Override
 			public Date call() throws Exception {
 				return sdf.parse("2020-06-12");
 			}
-		};*/
+		};*//*
 
 		//线程安全
 		Callable<Date> task = () -> DateFormatThreadLocal.convert("2020-06-12");
@@ -34,7 +36,34 @@ public class TestSimplDateFormat {
 		for (Future<Date> result : list) {
 			System.out.println(result.get());
 		}
-		
+
 		pool.shutdown();
+	}*/
+
+	public static void main(String[] args) throws ExecutionException, InterruptedException, ParseException {
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+		Callable<LocalDate> task = new Callable<LocalDate>() {
+		@Override
+		public LocalDate call() throws Exception {
+			return LocalDate.parse("2020-06-12", dtf);
+		}
+	};
+
+	List<Future<LocalDate>> list = new ArrayList<>();
+
+	ExecutorService pool = Executors.newFixedThreadPool(10);
+
+		for (int i = 0; i < 100; i++) {
+		list.add(pool.submit(task));
 	}
+
+		for (Future<LocalDate> result : list) {
+		System.out.println(result.get());
+	}
+
+		pool.shutdown();
+}
+
 }
